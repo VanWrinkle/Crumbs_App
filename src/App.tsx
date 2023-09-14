170821270a96a7f604d44ceb8f74704addae014e
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2023-09-14 22:18:39 +0200

    opprettet komponent for opprettelse av innlegg (må fortsatt styles)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
 import './SocialMediaPost.ts'
 import { SocialMediaPost, SocialMediaPostV1 } from "./SocialMediaPost";
+import {SyntheticEvent, useState} from "react";
 
 
 function App() {
   let staticPost = new SocialMediaPostV1("Guest", "Hey there! This is a social media post!");
   return (
-    <SocialMediaPostDisplayBrief post={staticPost} />
+      <>
+        <SocialMediaPostNew />
+        <SocialMediaPostDisplayBrief post={staticPost} />
+      </>
   );
 }
 
@@ -15,5 +19,23 @@ function SocialMediaPostDisplayBrief({post}: {post: SocialMediaPost}) {
       <p>{post.userId}: {post.content}</p>
   );
 }
+
+function SocialMediaPostNew() {
+  const [userInput, setUserInput] = useState("");
+  function onClick(e: SyntheticEvent) {
+      e.preventDefault();
+      let post = new SocialMediaPostV1("Guest", userInput);
+      setUserInput("");
+      alert("new object created (not stored):\n\n" + JSON.stringify(post, null, 2));
+  }
+
+  return (
+    <form onSubmit={onClick}>
+      <textarea value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+      <button type="submit">Post new crumb</button>
+    </form>
+  );
+}
+
 
 export default App;
