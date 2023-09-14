3ace726eefc90cc7ab97968c23e2ef0940410dab
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2023-09-14 20:57:29 +0200

    La til SocialMediaPost datastruktur

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './SocialMediaPost.ts'
+import { SocialMediaPost, SocialMediaPostV1 } from "./SocialMediaPost";
+
 
 function App() {
+  let staticPost = new SocialMediaPostV1("Guest", "Hey there! This is a social media post!");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SocialMediaPostDisplayBrief post={staticPost} />
+  );
+}
+
+function SocialMediaPostDisplayBrief({post}: {post: SocialMediaPost}) {
+  return (
+      <p>{post.userId}: {post.content}</p>
   );
 }
 
---
+++ src/SocialMediaPost.ts
@@ -0,0 +1,23 @@
+export interface SocialMediaPost {
+    userId: string;
+    published: Date;
+    expires: Date;
+    likes: number;
+    content: string;
+    comments: SocialMediaPost[];
+}
+
+export class SocialMediaPostV1 implements SocialMediaPost {
+    public published: Date;
+    public expires: Date;
+    public likes: number;
+    public comments: SocialMediaPostV1[];
+
+    constructor(public userId: string, public content: string) {
+        this.published = new Date();
+        this.expires = new Date();
+        this.expires.setDate(this.published.getDate() + 14);
+        this.likes = 0;
+        this.comments = [];
+    }
+}
