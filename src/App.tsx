bb4ab6bd72b058fc3e735a9bd918bdd7c304a62b
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2023-09-15 22:05:00 +0200

    basic styling med react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,36 @@ import './App.css';
 import './SocialMediaPost.ts'
 import { SocialMediaPost, SocialMediaPostV1, SocialMediaPostDispatch } from "./SocialMediaPost";
 import React, {SyntheticEvent, useState} from "react";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, Col, Container, Form, Image, Row, Button} from "react-bootstrap";
 
 function App() {
   let staticPost = new SocialMediaPostV1("Guest", "Hey there! This is a social media post!");
   const [socialPosts, setSocialPosts] = useState<SocialMediaPost[]>([staticPost])
 
   return (
-      <>
-        <SocialMediaPostNew socialPosts={socialPosts} setSocialPosts={setSocialPosts} />
-        <SocialMediaPostsDisplayAllBrief posts={socialPosts} />
-      </>
+     <Container className="main-content">
+      <Row>
+          <SocialMediaTopPanel socialPosts={socialPosts} setSocialPosts={setSocialPosts} />
+      </Row>
+      <Row>
+          <SocialMediaPostsDisplayAllBrief posts={socialPosts} />
+      </Row>
+     </Container>
   );
 }
 
-function SocialMediaPostsDisplayAllBrief({posts}: {posts: SocialMediaPost[]}) {
-    const results = posts.map((post) =>
-       <SocialMediaPostDisplayBrief post={post} />
-    );
+function SocialMediaTopPanel(props: {socialPosts: SocialMediaPost[], setSocialPosts: SocialMediaPostDispatch}) {
     return (
-        <div>{results}</div>
+        <>
+            <Col xs={3}>
+                <Image src="./logo.png" fluid />
+            </Col>
+            <Col>
+                <SocialMediaPostNew socialPosts={props.socialPosts} setSocialPosts={props.setSocialPosts} />
+            </Col>
+        </>
     );
-}
-
-function SocialMediaPostDisplayBrief({post}: {post: SocialMediaPost}) {
-  return (
-      <p>{post.userId}: {post.content}</p>
-  );
 }
 
 function SocialMediaPostNew(props: {socialPosts: SocialMediaPost[], setSocialPosts: SocialMediaPostDispatch}) {
@@ -37,17 +40,43 @@ function SocialMediaPostNew(props: {socialPosts: SocialMediaPost[], setSocialPos
       e.preventDefault();
       let post = new SocialMediaPostV1("Guest", userInput);
       setUserInput("");
-      alert("new object created (not stored):\n\n" + JSON.stringify(post, null, 2));
+      alert("DEBUG: new object created (not stored):\n\n" + JSON.stringify(post, null, 2));
       props.setSocialPosts([post, ...props.socialPosts]);
   }
 
   return (
-    <form onSubmit={onClick}>
-      <textarea value={userInput} onChange={(e) => setUserInput(e.target.value)} />
-      <button type="submit">Post new crumb</button>
-    </form>
+    <Form onSubmit={onClick} className="mb-3">
+        <Form.Control
+            as="textarea"
+            rows={3}
+            value={userInput}
+            placeholder="Write your crumb..."
+            className="mt-2 mb-2"
+            onChange={(e) => setUserInput(e.target.value)}>
+        </Form.Control>
+        <div className="d-grid">
+        <Button
+            type="submit"
+            variant="primary"
+            disabled={userInput.length === 0}>Post Crumb</Button>
+        </div>
+    </Form>
   );
 }
 
+function SocialMediaPostsDisplayAllBrief({posts}: {posts: SocialMediaPost[]}) {
+    const results = posts.map((post) =>
+        <SocialMediaPostDisplayBrief post={post} />
+    );
+    return (
+        <div>{results}</div>
+    );
+}
+
+function SocialMediaPostDisplayBrief({post}: {post: SocialMediaPost}) {
+    return (
+        <Card body className="mt-2 mb-2">{post.userId}: {post.content}</Card>
+    );
+}
 
 export default App;
