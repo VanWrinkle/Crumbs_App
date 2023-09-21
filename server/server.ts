86030ebfeb93fff1153a221da6f1babaee1bbee3
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2023-09-21 21:06:49 +0200

    added middleware function to redirect http to https

--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,32 @@
-import express, { Request, Response } from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import path from 'path';
 import https from 'https';
 import fs from 'fs';
+import http from "http";
 
+function requireHTTPS(req: Request, res: Response, next: NextFunction) {
+    if (!req.secure) {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const privateKey = fs.readFileSync('private-key.pem', 'utf-8');
 const certificate = fs.readFileSync('server.crt', 'utf-8');
-const credentials = { key: privateKey, cert: certificate };
 
 const app = express();
 const port = 443;
-const httpsServer = https.createServer(credentials, app).listen(port);
 const reactDir = path.join(__dirname, '..', 'client')
 
+
+app.use(requireHTTPS);
 app.use(express.static(path.join(reactDir, 'build')));
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (res: Response) => {
     res.sendFile(path.join(reactDir, '/build', 'index.html'))
 });
 
-console.log(`Server running at https://localhost:${port}`);
+http.createServer(app).listen(80);
+https.createServer({key: privateKey, cert: certificate}, app).listen(port, () => {
+    console.log(`Server running at https://localhost:${port}`);
+});
+
