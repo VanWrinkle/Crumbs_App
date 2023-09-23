c2b09b6d8934ce7902267c0b9853278c48a8e3cd
andrrma <andrrma@stud.ntnu.no>
2023-09-23 13:41:17 +0200

    Changes to utilize mock database until problem with IP is solved

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,7 @@ import fs from 'fs';
 // replace MongoDBService with this if not available
 // import {MockDatabase} from "./userDatabase/userDatabase";
 import {MongoUserDatabase} from "./userDatabase/mongoUserDatabase";
+import {MockUserDatabase} from "./userDatabase/mockUserDatabase";
 //import {MDBLoginService} from "./loginService/MDBLoginService";
 
 const privateKey = fs.readFileSync('private-key.pem', 'utf-8');
@@ -13,7 +14,7 @@ const certificate = fs.readFileSync('server.crt', 'utf-8');
 
 const app = express();
 
-app.use('/', require('./routes')(new MongoUserDatabase()));
+app.use('/', require('./routes')(new MockUserDatabase()));
 
 http.createServer(app).listen(80);
 https.createServer({key: privateKey, cert: certificate}, app).listen(443, () => {
