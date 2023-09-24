2860be428f1cde67cb84c07b7284dba872b44836
andrrma <andrrma@stud.ntnu.no>
2023-09-24 14:03:53 +0200

    Comments IUserDatabase.ts

--- server/src/userDatabase/IUserDatabase.ts
+++ server/src/userDatabase/IUserDatabase.ts
@@ -4,16 +4,22 @@ export interface StoredUserData {
     salt: string,
 }
 
+/**
+ * Interface for user databases containing username, hash and salt.
+ */
 export interface IUserDatabase {
     /**
-     *
-     * @param user - {StoredUserData}
+     * Adds a user to the database. Failing to add the user to the
+     * database due to technical problems raises an error.
+     * @param user - user to be added to storage
      */
     addUser(user: StoredUserData): Promise<void>
 
     /**
-     *
+     * Retrieves the data matching on username, otherwise undefined.
+     * An error is raised only if interaction with the database fails.
      * @param username
+     * @return - user data if match found, undefined otherwise
      */
     getUser(username: string): Promise<StoredUserData | undefined>
 }
