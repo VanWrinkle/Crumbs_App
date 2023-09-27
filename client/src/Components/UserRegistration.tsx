9421f6ec847299bc8f39b4e225767f24058cd59d
Even Stetrud <evenstet@stud.ntnu.no>
2023-09-27 11:57:57 +0200

    Installed axios; implemented api-call for user-registration

--- client/src/Components/UserRegistration.tsx
+++ client/src/Components/UserRegistration.tsx
@@ -1,5 +1,6 @@
-import React, {useState} from "react";
-import {Button, Card, Container, Form} from "react-bootstrap"
+import React, {SyntheticEvent, useState} from "react";
+import {Button, Card, Container, Form, FormGroup} from "react-bootstrap"
+import axios from "axios";
 
 export function UserRegistration() {
 
@@ -10,7 +11,7 @@ export function UserRegistration() {
     const validName = new RegExp("^(?=[a-z_]{4,30}$)");
     const validPassword = new RegExp("^(?=.*[a-zA-Z])(?=.*\\d).{8,}");
 
-    const validateName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const validateName = (event: any) => {
 
         if (!validName.test(event.target.value)) {
             setNameErr(true);
@@ -21,7 +22,7 @@ export function UserRegistration() {
         }
     }
 
-    const validatePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const validatePassword = (event: any) => {
         //event.preventDefault();
         if (!validPassword.test(event.target.value)) {
             setPasswordErr(true);
@@ -32,34 +33,61 @@ export function UserRegistration() {
         }
     }
 
-    const submit = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const submit = (event: React.MouseEvent) => {
         event.preventDefault();
+        console.log("You are here");
+        axios.post("https://localhost/api/register", {
+            username: name,
+            password: password
 
+        })
+            .then(function (response: any) {
+                console.log(response)
+            })
+            .catch(function (error: any) {
+                console.log(error)
+            });
     }
 
     return (
         <Card className={"main-content"}>
-            <Form className={"form-group"}>
-                <input
-                className={"form-control"}
-                type={"name"}
-                placeholder={"User name"}
-                //value={name}
-                onChange={e => validateName(e)}/>
-                {nameErr && <p>Your username is invalid; it must be between 4 and 30 characters long and can only contain lowercase letters</p>}
-                <input
-                className={"form-control"}
-                type={"password"}
-                placeholder={"Password"}
-                //value={password}
-                onChange={e => validatePassword(e)}/>
-                {passwordErr && <p>Your password is invalid; it must 8 characters or longer, and must contain at least one uppercase letter, one lowercase letter and one number</p>}
-                <Button
-                    disabled={(name.length === 0 || password.length === 0)||
-                    (nameErr || passwordErr)}
-                    type={"submit"}
-                    className={"btn"}
-                    onClick={e => submit}>Submit</Button>
+            <Form>
+                <Form.Group className={"m-3"}>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                        type={"text"}
+                        placeholder={"Username"}
+                        onChange={e => validateName(e)}
+                    />
+                    {nameErr &&
+                        <Form.Text>
+                            Your username is invalid; it must be between 4 and 30 characters long and can only contain lowercase letters
+                        </Form.Text>
+                        }
+                </Form.Group>
+
+                <Form.Group className={"m-3"}>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type={"password"}
+                        placeholder={"Password"}
+                        onChange={e => validatePassword(e)}
+                    />
+                    {passwordErr &&
+                        <Form.Text>
+                            Your password is invalid; it must 8 characters or longer, and must contain at least one uppercase letter, one lowercase letter and one number
+                        </Form.Text>
+                    }
+                </Form.Group>
+
+                <Form.Group className={"m-3"}>
+                    <Button
+                        disabled={(name.length === 0 || password.length === 0)||
+                        (nameErr || passwordErr)}
+                        type={"submit"}
+                        className={"btn-dark"}
+                        onClick={(e) => submit(e)}>Submit</Button>
+                </Form.Group>
             </Form>
         </Card>
     )
