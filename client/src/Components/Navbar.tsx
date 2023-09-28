eb2224118e230f9adf3e16cceed7b925d7578958
Even Stetrud <evenstet@stud.ntnu.no>
2023-09-28 14:13:11 +0200

    Implemented collapsible navbar, with dropdown-login

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -1,14 +1,21 @@
-import {Container, Navbar} from "react-bootstrap"
+import {Container, Navbar, Nav, NavDropdown} from "react-bootstrap"
 import { Outlet } from "react-router-dom";
+import UserLogin from "./UserLogin";
 
 export function CrumbNavbar() {
     return(
         <>
-            <Navbar className={"navbar sticky-top"}>
-                <Container className={"bg-body-secondary border-1 main-content"}>
-                    <Navbar.Brand href={"/"}>Personal feed</Navbar.Brand>
-                    <Navbar.Brand href={"signup"}>Signup</Navbar.Brand>
-                    <Navbar.Brand href={"login"}>Log in</Navbar.Brand>
+            <Navbar expand={"lg"} className={"navbar sticky-top"}>
+                <Container className={"main-content"}>
+                    <Navbar.Brand href={"/"}>Crumbs</Navbar.Brand>
+                    <Navbar.Toggle aria-controls={"basic-navbar-nav"}/>
+                    <Navbar.Collapse role={""} id={"basic-navbar-nav"}>
+                        <Nav>
+                            <NavDropdown title={"Log in"}>
+                                <UserLogin/>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
             <Outlet />
