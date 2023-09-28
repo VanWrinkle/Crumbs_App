a8ea211316c2d764253463c6d5a3ac5a27c2ac1f
andrrma <andrrma@stud.ntnu.no>
2023-09-28 09:37:42 +0200

    Modifies test script argument

--- client/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders main page', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/signup/i);
-  expect(linkElement).toBeInTheDocument();
-});
