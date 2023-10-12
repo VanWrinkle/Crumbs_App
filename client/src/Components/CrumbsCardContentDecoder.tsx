eac99414ebedb2b8fc40aa79b911a8add8fd0b2b
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2023-10-12 18:36:05 +0200

    more front-end refactorization

---
+++ client/src/Components/CrumbsCardContentDecoder.tsx
@@ -0,0 +1,32 @@
+import {Link} from "react-router-dom";
+import React from "react";
+import {CrumbContent} from "../types/Crumb";
+
+export function CrumbsCardContentDecoder({contents}: {contents: CrumbContent[]}) {
+    return(
+        <>
+        {contents.map((item) =>
+            item.type == "hashtag" ? <CrumbCardHashtag content={item} /> :
+            item.type == "mention" ? <CrumbCardMention content={item} /> :
+            <>{item.value}</>
+        )}
+        </>
+    )
+}
+
+function CrumbCardHashtag({content}: {content: CrumbContent}) {
+    return(
+        <Link
+            to={""}
+            style={{color: "inherit"}}>{content.value}
+        </Link>
+    )
+}
+
+function CrumbCardMention({content}: {content: CrumbContent}) {
+    return (
+        <Link
+            to={`/profile/${content.value.substring(1)}`}
+            style={{color: "inherit"}}>{content.value}
+        </Link>)
+}
