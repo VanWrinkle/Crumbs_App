826b84292ac82cd5bd274d5f670b9e20ca34dcff
Øystein Qvigstad <oysteinq@stud.ntnu.no>
2023-10-13 15:19:17 +0200

    fixed bug with drop down menu alignment for smaller screens

--- client/src/pages/Frontpage.tsx
+++ client/src/pages/Frontpage.tsx
@@ -5,7 +5,7 @@ import {Feed} from "../containers/Feed";
 
 export function Frontpage({feedBulkSize}: {feedBulkSize: number}) {
     return (
-        <Container>
+        <Container fluid>
             <Feed
                 canCompose={true}
                 feed={(continueFrom: string) => new Api().getMainFeed(feedBulkSize, continueFrom)}
