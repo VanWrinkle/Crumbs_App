34eefd9c9f54ec1806518584f24aafff68d83eef
andrrma <andrrma@stud.ntnu.no>
2023-11-09 16:35:04 +0100

    Adds comments to class methods

--- server/src/user/login/authentication/AuthenticationService/AuthenticationService.ts
+++ server/src/user/login/authentication/AuthenticationService/AuthenticationService.ts
@@ -11,6 +11,11 @@ export class AuthenticationService implements IUserAuthenticationService {
         this.#expirationTimeMilliseconds = expirationHours * 60 * 60 * 1000;
     }
 
+    /**
+     * tokenParser attempts to verify a token attached to the incoming request as a cookie.
+     * If there is no token, or the verification fails, the next middleware is called.
+     * If it is present and verifies successfully, user is set on the request.
+     */
     tokenParser() {
         return (req: express.Request, _res: express.Response, next: express.NextFunction) => {
             const token = req.cookies['token'];
@@ -27,6 +32,13 @@ export class AuthenticationService implements IUserAuthenticationService {
         }
     }
 
+
+    /**
+     * Sends a JSON web token (JWT) to the client along with some additional data in the response.
+     *
+     * @param username - The username to include in the token's payload.
+     * @param response - The Express response object used to send the token and response data.
+     */
     sendToken(username: string, response: express.Response) {
         const now = new Date()
         const payload = { username: username, ttl: new Date(now.getTime() + this.#expirationTimeMilliseconds) }
@@ -42,6 +54,12 @@ export class AuthenticationService implements IUserAuthenticationService {
         response.status(200).json(payload)
     }
 
+
+    /**
+     * Clears the session token cookie in the response and sends a confirmation message.
+     *
+     * @param response - The Express response object used to clear the token cookie and send a response.
+     */
     clearSessionToken(response: express.Response) {
         response.clearCookie('token', {httpOnly: true, secure: true})
         response.status(200).send('token cleared')
