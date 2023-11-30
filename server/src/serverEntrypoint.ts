9b02a4d4ebbd8fb0247947718418ec6ce788f1a8
andrrma <andrrma@stud.ntnu.no>
2023-11-30 10:06:05 +0100

    Updates with initial logging functionality and refactors instantiation of app versions for testing

--- server/src/serverEntrypoint.ts
+++ server/src/serverEntrypoint.ts
@@ -1,33 +1,29 @@
-import fs from 'fs';
-import {MDBUserRegistrationDatabase} from "./user/registration/persistence/MDBUserRegistrationDatabase/MDBUserRegistrationDatabase";
 import {LoginService} from "./user/login/LoginService/LoginService";
 import {RegistrationService} from "./user/registration/RegistrationService/RegistrationService";
 import {CrumbServer} from "./server/crumbServer";
 import {ConfigSettings} from "./entities/ConfigSettings";
 import {AuthenticationService} from "./user/login/authentication/AuthenticationService/AuthenticationService";
-import {NeoGraphPersistence} from "./user/content/socialGraph/NeoGraphPersistence/NeoGraphPersistence";
 import {
     httpsCertificate,
     httpsPrivateKey,
     socialGraphPersistence,
     userRegistrationDatabase,
 } from "./globals";
-import {TestServerConfigs} from "./testUtility/testServerConfig";
 
 
-// let user_db = userRegistrationDatabase();
-// let graph_db = socialGraphPersistence();
-//
-// const sessionManagement = new AuthenticationService('secret-key', 24)
-//
-// const config: ConfigSettings = {
-//     registrationService: new RegistrationService(user_db, graph_db),
-//     loginService: new LoginService(user_db, sessionManagement),
-//     graphPersistence: graph_db,
-//     httpsPrivateKey: httpsPrivateKey,
-//     httpsCertificate: httpsCertificate,
-// }
+let user_db = userRegistrationDatabase();
+let graph_db = socialGraphPersistence();
 
-const app = TestServerConfigs.graphDBErrorServer();//new CrumbServer(config);
+const sessionManagement = new AuthenticationService('secret-key', 24)
+
+const config: ConfigSettings = {
+    registrationService: new RegistrationService(user_db, graph_db),
+    loginService: new LoginService(user_db, sessionManagement),
+    graphPersistence: graph_db,
+    httpsPrivateKey: httpsPrivateKey,
+    httpsCertificate: httpsCertificate,
+}
+
+const app = new CrumbServer(config);
 
 app.run();
